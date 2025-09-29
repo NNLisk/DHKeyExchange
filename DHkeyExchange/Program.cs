d815d25842572306fdc50286cd06f1cbe7050b04
NNLisko <niko.1lausto2@gmail.com>
2025-09-29 22:05:43 +0300

    utility class

---
+++ DHkeyExchange/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Numerics;
+using DiffieHellman;
+
+class Program
+{
+    static void Main()
+    {
+        BigInteger prime = DHkeyUtil.GetPrime();
+        int generator = DHkeyUtil.GetGenerator();
+
+        Console.WriteLine($"Prime: {prime}");
+        Console.WriteLine($"Generator: {generator}");
+    }
+}
